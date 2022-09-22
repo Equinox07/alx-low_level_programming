2a777c2b91951ffc9c549828915d71b315b907d9
Hope Avalon <hopegag@gmail.com>
2022-09-21 21:13:33 -0700

    print reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
