2abe17bc5c2c9ba274175900242b30ccd6f4e700
Hope Avalon <hopegag@gmail.com>
2022-09-21 21:44:53 -0700

    print number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - combinations of single-digit numbers.
+* followed by a new line.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+
+if (i != 57)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+return (0);
+}
